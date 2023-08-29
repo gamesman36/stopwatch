fb6d54b649e3ba86f068ec2e24cd26e6caa5898b
Daniel Andreas Wang <gamesman36@outlook.com>
2023-08-29 12:28:13 +0200

    Correcting calculation for minutes

--- script.js
+++ script.js
@@ -9,39 +9,42 @@ let secondsPassed;
 
 zero();
 function zero() {
-    times.innerHTML = "00:00:00";
-    secondsPassed = 0;
+  times.innerHTML = "00:00:00";
+  secondsPassed = 0;
 }
 
 function count() {
-    secondsPassed++;
+  secondsPassed++;
 
-    // Conversion from seconds to HH:MM:SS
+  // Conversion from seconds to HH:MM:SS
 
-    let hours = Math.floor(secondsPassed / 60 / 60);
-    let minutes = Math.floor(secondsPassed / 60) - (hours * 60);
-    let seconds = secondsPassed % 60;
+  let hours = Math.floor(secondsPassed / 60 / 60);
+  let minutes = Math.floor((secondsPassed / 60) % 60);
+  let seconds = secondsPassed % 60;
 
-    formattedTime = hours.toString().padStart(2, '0') + ':' +
-        minutes.toString().padStart(2, '0') + ':' +
-        seconds.toString().padStart(2, '0');
+  formattedTime =
+    hours.toString().padStart(2, "0") +
+    ":" +
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
 
-    times.innerHTML = formattedTime;
+  times.innerHTML = formattedTime;
 }
 
 function start() {
-    if (clicked) return;
-    clicked = true;
-    setInterval(count, 1000);
+  if (clicked) return;
+  clicked = true;
+  setInterval(count, 1000);
 }
 
 function lap() {
-    if (!clicked) return;
-    laps.innerHTML += `${formattedTime}<br />`;
-    clearInterval(timer);
-    zero();
+  if (!clicked) return;
+  laps.innerHTML += `${formattedTime}<br />`;
+  clearInterval(timer);
+  zero();
 }
 
 function reload() {
-    location.reload();
+  location.reload();
 }
